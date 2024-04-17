dcd3fe1b4dc06c3173afbe22815452ea8d786425
mhamzao3 <167357345+mhamzao3@users.noreply.github.com>
2024-04-18 01:04:45 +0500

    Update Task15.ts

--- Task15.ts
+++ Task15.ts
@@ -1,6 +1,6 @@
-// Name =Nazir 
-// Today Date = 12/7/2023
-let friend_guest:string[] =["Mohsin","Bilal","Ali"];
+// Name =Muhammad
+// Today Date = 4/6/2024
+let friend_guest:string[] =["Rafeeq","Sami","Ali"];
 for(let i=0;i<friend_guest.length;i++)
 {
     if(friend_guest[i]=='Ali')
@@ -9,7 +9,7 @@ console.log("Ali not invited to dinner")
 //  replacing new frined invite list 
 //  splice first index show 
 // splice second 1 delete if 0 then no delete 
-friend_guest.splice(3,0,"Nazir")
+friend_guest.splice(3,0,"Muhammad")
     }
     else
     {
